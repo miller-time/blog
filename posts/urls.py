ef81505e86f4d5256323352df832b8b87b3ac1b7
Russell <miller.time.baby@gmail.com>
2010-12-23 13:02:50 -0800

    i think archive calendar is functional. rebooting.

--- posts/urls.py
+++ posts/urls.py
@@ -6,4 +6,11 @@ admin.autodiscover()
 urlpatterns = patterns('blog.posts.views',
     (r'^$', 'index'),
     (r'^archive/$', 'archive'),
+    (r'^archive/jun2010/$', 'archive_jun2010'),
+    (r'^archive/jul2010/$', 'archive_jul2010'),
+    (r'^archive/aug2010/$', 'archive_aug2010'),
+    (r'^archive/sep2010/$', 'archive_sep2010'),
+    (r'^archive/oct2010/$', 'archive_oct2010'),
+    (r'^archive/nov2010/$', 'archive_nov2010'),
+    (r'^archive/dec2010/$', 'archive_dec2010'),
 )
--- posts/views.py
+++ posts/views.py
@@ -6,5 +6,32 @@ def index(request):
     return render_to_response('posts/index.html', {'latest_blogs_list': latest_blogs_list})
 
 def archive(request):
-    latest_blogs_list = Post.objects.all().order_by('pub_date')
-    return render_to_response('posts/archive.html', {'latest_blogs_list': latest_blogs_list})
+    return render_to_response('posts/archive.html', )
+
+def archive_jun2010(request):
+    jun2010 = Post.objects.filter(pub_date__year=2010, pub_date__month=6)
+    return render_to_response('posts/jun2010.html', {'jun2010': jun2010})
+
+def archive_jul2010(request):
+    jul2010 = Post.objects.filter(pub_date__year=2010, pub_date__month=7)
+    return render_to_response('posts/jul2010.html', {'jul2010': jul2010})
+
+def archive_aug2010(request):
+    aug2010 = Post.objects.filter(pub_date__year=2010, pub_date__month=8)
+    return render_to_response('posts/aug2010.html', {'aug2010': aug2010})
+
+def archive_sep2010(request):
+    sep2010 = Post.objects.filter(pub_date__year=2010, pub_date__month=9)
+    return render_to_response('posts/sep2010.html', {'sep2010': sep2010})
+
+def archive_oct2010(request):
+    oct2010 = Post.objects.filter(pub_date__year=2010, pub_date__month=10)
+    return render_to_response('posts/oct2010.html', {'oct2010': oct2010})
+
+def archive_nov2010(request):
+    nov2010 = Post.objects.filter(pub_date__year=2010, pub_date__month=11)
+    return render_to_response('posts/nov2010.html', {'nov2010': nov2010})
+
+def archive_dec2010(request):
+    dec2010 = Post.objects.filter(pub_date__year=2010, pub_date__month=12)
+    return render_to_response('posts/dec2010.html', {'dec2010': dec2010})

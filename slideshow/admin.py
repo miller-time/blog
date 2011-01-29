bdcde0003759b25cd8de7402b7e6c84fb9cd85a9
Russell <miller.time.baby@gmail.com>
2011-01-29 13:17:42 -0800

    file got missed in last commit?

--- slideshow/admin.py
+++ slideshow/admin.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 class SlideAdmin(admin.ModelAdmin):
     filename = 'filename'
     date_added = 'date_added'
+    list_display = ['filename','date_added']
 
 admin.site.register(Slide, SlideAdmin) 
